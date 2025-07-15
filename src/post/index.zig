8a9d2e2ad12e71178b52520083ec773d284a6e64
Tom Speak <tom@speak.sh>
2025-07-15 23:33:44 +0100

    better memory layout for post init, arena where makes sense, gpa for Post

--- src/post/index.zig
+++ src/post/index.zig
@@ -11,22 +11,10 @@ pub const PostConfig = struct {
     published: bool,
 
     fn deinit(self: *PostConfig, allocator: std.mem.Allocator) void {
-        if (self.url) |x| {
-            allocator.free(x);
-            self.url = null;
-        }
-        if (self.date) |x| {
-            allocator.free(x);
-            self.date = null;
-        }
-        if (self.title) |x| {
-            allocator.free(x);
-            self.title = null;
-        }
-        if (self.description) |x| {
-            allocator.free(x);
-            self.description = null;
-        }
+        if (self.url) |x| allocator.free(x);
+        if (self.title) |x| allocator.free(x);
+        if (self.date) |x| allocator.free(x);
+        if (self.description) |x| allocator.free(x);
     }
 };
 pub const ConfigOptions = enum { url, date, title, description, published, unknown };
@@ -34,58 +22,54 @@ pub const ConfigOptions = enum { url, date, title, description, published, unkno
 pub const Post = @This();
 
 config: PostConfig,
-html: []u8,
+html: ?[]u8,
 
 pub fn new() Post {
     return .{
         .config = undefined,
-        .html = undefined,
+        .html = null,
     };
 }
 
-pub fn init(allocator: std.mem.Allocator, dir_path: []const u8) ![]Post {
-    const cwd = std.fs.cwd();
+pub fn init(gpa: std.mem.Allocator, dir_path: []const u8) ![]Post {
+    var arena = std.heap.ArenaAllocator.init(gpa);
+    defer arena.deinit();
+    const aa = arena.allocator();
+
+    var cwd = std.fs.cwd();
 
     var dir = try cwd.openDir(dir_path, .{ .iterate = true });
     defer dir.close();
 
-    var walker = try dir.walk(allocator);
+    var walker = try dir.walk(aa);
     defer walker.deinit();
 
-    var paths = std.ArrayListUnmanaged([]u8).empty;
-    defer paths.deinit(allocator);
+    var paths = std.ArrayListUnmanaged([]const u8).empty;
+    defer paths.deinit(aa);
     while (try walker.next()) |entry| {
         if (!std.mem.endsWith(u8, entry.path, ".md")) continue;
 
-        const full_path = try std.fs.path.join(allocator, &.{ dir_path, entry.path });
-        defer allocator.free(full_path);
-
-        const path_copy = try allocator.dupe(u8, full_path);
-        try paths.append(allocator, path_copy);
+        const full_path = try std.fs.path.join(aa, &.{ dir_path, entry.path });
+        try paths.append(aa, full_path);
     }
 
     var posts = std.ArrayListUnmanaged(Post).empty;
-    defer {
-        posts.deinit(allocator);
-        for (paths.items) |p| {
-            allocator.free(p);
-        }
-    }
+    defer posts.deinit(gpa);
     for (paths.items) |p| {
-        const buf = try std.fs.cwd().readFileAllocOptions(allocator, p, 1024 * 32, null, @alignOf(u8), 0);
-        defer allocator.free(buf);
+        const buf = try std.fs.cwd().readFileAllocOptions(gpa, p, 1024 * 32, null, @alignOf(u8), 0);
 
         var post = Post.new();
-        try Markdown.parse(allocator, &post, buf);
+        try Markdown.parse(gpa, &post, buf);
 
-        try posts.append(allocator, post);
+        try posts.append(gpa, post);
+        gpa.free(buf);
     }
 
-    return posts.toOwnedSlice(allocator);
+    return posts.toOwnedSlice(gpa);
 }
 
 pub fn deinit(self: *Post, allocator: std.mem.Allocator) void {
-    allocator.free(self.html);
+    if (self.html) |x| allocator.free(x);
     self.config.deinit(allocator);
 }
 
@@ -96,7 +80,7 @@ pub fn debug(self: *Post) void {
         optStr(self.config.title),
         optStr(self.config.description),
         self.config.published,
-        self.html,
+        optStr(self.html),
     });
 }
 
